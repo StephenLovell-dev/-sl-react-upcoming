75dfe704fb985dc6851db308b03546a1acca6791
Stephen Lovell <stephen.lovell@bbc.co.uk>
2024-09-04 17:10:32 +0100

    fix: first commit

---
+++ src/test_data/nexts.js
@@ -0,0 +1,340 @@
+export const data = {
+    next: [
+      {
+          "start": "2024-02-16T12:37:04Z",
+          "duration": "PT30S",
+          "vpid": "p0h5by8y",
+          "source": "sched",
+          "pid": "p0h5bxdz",
+          "entity_type": "episode",
+          "title": "Murder Trial: The Killing of Dr Brenda Page",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "title": {
+                          "$": "Murder Trial: The Killing of Dr Brenda Page"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:brand",
+                              "pid": "p00b0hg9"
+                          }
+                      },
+                      "title": {
+                          "$": "BBC Trailers"
+                      }
+                  }
+              ]
+          }
+      },
+      {
+          "start": "2024-02-16T12:37:34Z",
+          "duration": "PT59M0.04S",
+          "vpid": "m001ttqw",
+          "source": "sched",
+          "pid": "m001ttqx",
+          "entity_type": "episode",
+          "title": "Series 11 - A Norman Panic Room and a Mesolithic Fish Trap",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "title": {
+                          "$": "A Norman Panic Room and a Mesolithic Fish Trap"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:series",
+                              "pid": "m001tmmw"
+                          }
+                      },
+                      "title": {
+                          "$": "Series 11"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:brand",
+                              "pid": "b014hl0d"
+                          }
+                      },
+                      "title": {
+                          "$": "Digging for Britain"
+                      }
+                  }
+              ]
+          }
+      },
+      {
+          "source": "sched",
+          "title": "Series 11 - A Roman Mystery and Waterloo’s Disappearing Dead",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "title": {
+                          "$": "A Roman Mystery and Waterloo’s Disappearing Dead"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:series",
+                              "pid": "m001tmmw"
+                          }
+                      },
+                      "title": {
+                          "$": "Series 11"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:brand",
+                              "pid": "b014hl0d"
+                          }
+                      },
+                      "title": {
+                          "$": "Digging for Britain"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T13:36:34Z",
+          "duration": "PT59M0.04S",
+          "vpid": "m001ttqs",
+          "entity_type": "episode",
+          "epid": "m001ttqr"
+      },
+      {
+          "source": "sched",
+          "title": "The Traitors",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "title": {
+                          "$": "The Traitors"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:brand",
+                              "pid": "p00b0hg9"
+                          }
+                      },
+                      "title": {
+                          "$": "BBC Trailers"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T14:35:35Z",
+          "duration": "PT39S",
+          "vpid": "p0gsw6kx",
+          "entity_type": "episode",
+          "epid": "p0gsw65l"
+      },
+      {
+          "source": "sched",
+          "title": "Climbing Great Buildings - Blenheim Palace",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "presentation_title": {
+                          "$": "Episode 8"
+                      },
+                      "title": {
+                          "$": "Blenheim Palace"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:series",
+                              "pid": "b00ts2sg"
+                          }
+                      },
+                      "title": {
+                          "$": "Climbing Great Buildings"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T14:36:14Z",
+          "duration": "PT28M58.8S",
+          "vpid": "b00tv97g",
+          "entity_type": "episode",
+          "epid": "b00tv99n"
+      },
+      {
+          "source": "sched",
+          "title": "Climbing Great Buildings - Clifton Suspension Bridge",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "presentation_title": {
+                          "$": "Episode 9"
+                      },
+                      "title": {
+                          "$": "Clifton Suspension Bridge"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:series",
+                              "pid": "b00ts2sg"
+                          }
+                      },
+                      "title": {
+                          "$": "Climbing Great Buildings"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T15:05:12Z",
+          "duration": "PT28M40.04S",
+          "vpid": "b00ty40c",
+          "entity_type": "episode",
+          "epid": "b00ty47v"
+      },
+      {
+          "source": "sched",
+          "title": "Climbing Great Buildings - St Pancras",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "presentation_title": {
+                          "$": "Episode 10"
+                      },
+                      "title": {
+                          "$": "St Pancras"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:series",
+                              "pid": "b00ts2sg"
+                          }
+                      },
+                      "title": {
+                          "$": "Climbing Great Buildings"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T15:33:52Z",
+          "duration": "PT28M59.04S",
+          "vpid": "b00ty48f",
+          "entity_type": "episode",
+          "epid": "b00ty4v1"
+      },
+      {
+          "source": "sched",
+          "title": "Climbing Great Buildings - Glasgow School of Art",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "presentation_title": {
+                          "$": "Episode 11"
+                      },
+                      "title": {
+                          "$": "Glasgow School of Art"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:series",
+                              "pid": "b00ts2sg"
+                          }
+                      },
+                      "title": {
+                          "$": "Climbing Great Buildings"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T16:02:51Z",
+          "duration": "PT28M59.04S",
+          "vpid": "m001qp0t",
+          "entity_type": "episode",
+          "epid": "b00ty4wj"
+      },
+      {
+          "source": "sched",
+          "title": "Dragons' Den",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "title": {
+                          "$": "Dragons' Den"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:brand",
+                              "pid": "p00b0hg9"
+                          }
+                      },
+                      "title": {
+                          "$": "BBC Trailers"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T16:31:50Z",
+          "duration": "PT15S",
+          "vpid": "p0gww938",
+          "entity_type": "episode",
+          "epid": "p0gww8jz"
+      },
+      {
+          "source": "sched",
+          "title": "Series 11 - 3,000-Year-Old Shoes and Giant Axeheads",
+          "title_hierarchy": {
+              "titles": [
+                  {
+                      "title": {
+                          "$": "3,000-Year-Old Shoes and Giant Axeheads"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:series",
+                              "pid": "m001tmmw"
+                          }
+                      },
+                      "title": {
+                          "$": "Series 11"
+                      }
+                  },
+                  {
+                      "inherited_from": {
+                          "link": {
+                              "rel": "pips-meta:brand",
+                              "pid": "b014hl0d"
+                          }
+                      },
+                      "title": {
+                          "$": "Digging for Britain"
+                      }
+                  }
+              ]
+          },
+          "start": "2024-02-16T16:32:05Z",
+          "duration": "PT59M0.04S",
+          "vpid": "m001ttqq",
+          "entity_type": "episode",
+          "epid": "m001ttqp"
+      }
+  ]
+  };
+
